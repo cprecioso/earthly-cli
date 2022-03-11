3bb465aa31f39aaf2aadf98aa5f17e9d816928c3
Carlos Precioso <511681+cprecioso@users.noreply.github.com>
2022-03-12 00:16:41 +0100

    Run all deploys and then fail

--- src/publish.mjs
+++ src/publish.mjs
@@ -1,18 +1,27 @@
 // @ts-check
 
 import { execa } from "execa";
+import PQueue from "p-queue";
+
+const queue = new PQueue({ concurrency: 1 });
 
 export const publish = async function* (
   /** @type {AsyncIterable<import("vinyl")>} */ files
 ) {
+  const promises = [];
+
   for await (const file of files) {
-    try {
-      await execa("npm", ["publish", file.path], {
+    const promise = queue.add(() =>
+      execa("npm", ["publish", file.path], {
         stdout: "inherit",
         stderr: "inherit",
-      });
-    } catch {
-      console.log("FAILED");
-    }
+      })
+    );
+    promises.push(promise);
   }
+
+  // Wait until we do everything
+  await Promise.allSettled(promises);
+  // Then throw an error if any failed
+  await Promise.all(promises);
 };
